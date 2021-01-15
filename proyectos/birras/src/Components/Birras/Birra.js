18212f474934658d776beea2be3de524091e3402
juliog <julio.gorsd@gmail.com>
2021-01-15 17:42:15 -0300

    css file deleted, styles includes on js file

--- proyectos/birras/src/Components/Birras/Birra.js
+++ proyectos/birras/src/Components/Birras/Birra.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReplyAll } from '@fortawesome/free-solid-svg-icons';
 import {useState} from 'react';
 import ReactCardFlip from 'react-card-flip';
-import "./birra.css";
+
 const Birra = ({abv,description,first_brewed,ibu,id,image_url,name,tagline,food_pairing}) => {
     const [isFlipped,setIsFlipped]=useState(false);
     const handleClick=() =>{
@@ -14,9 +14,9 @@ const Birra = ({abv,description,first_brewed,ibu,id,image_url,name,tagline,food_
         <Col md={4} className="mt-3 mb-3 text-centerr">
             <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
 
-               <Card className="contenedor"> 
+               <Card style={{width:"350px",height:"560px"}}> 
                 <Card.Header style={{backgroundColor:'#f88f01',textAlign:"center",color:"#fff",fontSize:"18px",fontWeight:"700"}}>{name}</Card.Header>
-                <Card.Img variant={"top"} src={image_url} className="mx-auto  w-50 d-block img-fluid photo"></Card.Img>
+                <Card.Img variant={"top"} src={image_url} className="mx-auto  w-50 d-block img-fluid" style={{maxHeight:"250px",maxWidth:"250px",objectFit:"scale-down",paddingTop:"20px"}}></Card.Img>
                 
                 <Card.Body>
                     <Card.Title style={{color:"#f88f01",textAlign:"center",marginTop:"30px"}}>{tagline}</Card.Title>
@@ -29,8 +29,8 @@ const Birra = ({abv,description,first_brewed,ibu,id,image_url,name,tagline,food_
                         
                 </Card.Footer>
             </Card> 
-            <Card className="contenedor">
-            <Card.Header className="text-center" style={{backgroundColor:'#f88f01'}}>{name}</Card.Header>
+            <Card style={{width:"350px",height:"560px"}}>
+            <Card.Header className="text-center" style={{backgroundColor:'#b6ed11',fontWeight:"700",fontSize:"18px",color:"white"}}>{name}</Card.Header>
 
                 <Card.Body>
                 
@@ -49,7 +49,7 @@ const Birra = ({abv,description,first_brewed,ibu,id,image_url,name,tagline,food_
                     </Card.Text>
                     
                 </Card.Body>
-                <Card.Footer style={{backgroundColor:'#f88f01'}}>
+                <Card.Footer style={{backgroundColor:'#b6ed11'}}>
                         <Button  variant='link' onClick={handleClick} size="sm" ><FontAwesomeIcon icon={faReplyAll} size="2x" color="white"  /></Button>
                 </Card.Footer>
             </Card> 
